5125865da9916b0f724a2f96a88bea11b84ac28c
mshashank02 <mshashank02@gmail.com>
2023-08-28 17:41:40 +0200

    added csv

--- include/path_follow.hpp
+++ include/path_follow.hpp
@@ -54,8 +54,8 @@ std::vector<Eigen::Vector2d> get_drone_velocity_setpoint(std::vector<Eigen::Vect
                 drone_setpoint_error_y = 0;
             }
             std::cout<<"setpoint error:"<<drone_setpoint_error_x<<","<<drone_setpoint_error_y<<std::endl;
-            drone_setpoint_error[i][0] = drone_setpoint_error_x/std::sqrt(pow(drone_setpoint_error_x,2) + pow(drone_setpoint_error_y,2));
-            drone_setpoint_error[i][1] = drone_setpoint_error_y/std::sqrt(pow(drone_setpoint_error_x,2) + pow(drone_setpoint_error_y,2));
+            drone_setpoint_error[i][0] = drone_setpoint_error_x/std::sqrt(drone_setpoint_error_x*drone_setpoint_error_x + drone_setpoint_error_y*drone_setpoint_error_y);
+            drone_setpoint_error[i][1] = drone_setpoint_error_y/std::sqrt(drone_setpoint_error_x*drone_setpoint_error_x + drone_setpoint_error_y*drone_setpoint_error_y);
             std::cout<<"added errors insidn\n";
             drone_velocity_setpoint[i] = kp*drone_setpoint_error[i] ;
             std::cout<<"got setpoints:"<<drone_velocity_setpoint[i]<<std::endl;
